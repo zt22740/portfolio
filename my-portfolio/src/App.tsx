5cdc23b40767a802a79eaa6dd571ec6645fa9c42
ykmhere <zt22740@bristol.ac.uk>
2025-02-12 22:24:23 +0000

    made everything be beside (below) the side bar

--- my-portfolio/src/App.tsx
+++ my-portfolio/src/App.tsx
@@ -1,22 +1,24 @@
-import './App.css'
-import Navbar from './components/navbar'
-import Sidebar from './components/sidebar'
-import About from './components/about'
-import Projects from './components/projects'
-import Contact from './components/contact'
-import Footer from './components/footer'
+import './App.css';
+import Navbar from './components/navbar';
+import Sidebar from './components/sidebar';
+import About from './components/about';
+import Projects from './components/projects';
+import Contact from './components/contact';
+import Footer from './components/footer';
 
 function App() {
   return (
     <>
-      <Navbar />
       <Sidebar />
-      <About />
-      <Projects />
-      <Contact />
+      <Navbar />
+      <div className="content-container">
+        <About />
+        <Projects />
+        <Contact />
+      </div>
       <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- my-portfolio/src/components/navbar.tsx
+++ my-portfolio/src/components/navbar.tsx
@@ -1,15 +1,17 @@
+import "../styles/navbar.css";
+
 const Navbar = () => {
-    return (
-      <nav className="w-3/4 mx-auto flex justify-center space-x-6 py-4 text-white">
-        <a href="#home" className="hover:text-purple-400">Home</a>
-        <a href="#portfolio" className="hover:text-purple-400">Portfolio</a>
-        <a href="#about" className="hover:text-purple-400">About Me</a>
-        <a href="#resume" className="hover:text-purple-400">Resume</a>
-        <a href="#contact" className="hover:text-purple-400">Contact</a>
-        <button className="ml-4 bg-purple-500 px-4 py-1 rounded-lg">Let's Talk</button>
-      </nav>
-    );
-  };
-  
-  export default Navbar;
-  
+  return (
+    <nav className="navbar">
+      <ul className="nav-links">
+        <li><a href="#home">Home</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#about">About Me</a></li>
+        <li><a href="#resume">Resume</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
